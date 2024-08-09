ab8f394fa359456ed879c139a236488e170c6887
AAArmine <aaarmine.aghababyan@gmail.com>
2024-08-10 02:40:04 +0400

    about us, team carousel

--- src/components/sections/about/TeamImages.vue
+++ src/components/sections/about/TeamImages.vue
@@ -1,17 +1,92 @@
 <template>
-  <section class="carusel">
+  <section class="images">
     <div class="wrapper-80">
-      
+      <h1 class="title">Mød vores team</h1>
+      <Carousel v-bind="settings">
+        <Slide v-for="member in teamMembers" :key="member.id">
+          <div class="team-member">
+            <img
+              :src="require(`@/assets/images/${member.image}`)"
+              alt="Team Member"
+              class="member-image"
+            />
+            <div class="member-info">
+              <h3 class="member-name">{{ member.name }}</h3>
+              <p class="member-position">{{ member.position }}</p>
+            </div>
+          </div>
+        </Slide>
+
+        <template #addons>
+          <Navigation />
+        </template>
+      </Carousel>
     </div>
   </section>
 </template>
 
 <script>
-export default {
+import { defineComponent } from "vue";
+import { Carousel, Navigation, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+
+export default defineComponent({
   name: "TeamImages",
-};
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  },
+  data() {
+    return {
+      teamMembers: [
+        { id: 1, image: "main.png", name: "Hrayr", position: "Programmer" },
+        {
+          id: 2,
+          image: "main2.png",
+          name: "Server Expert",
+          position: "Server Specialist",
+        },
+        {
+          id: 3,
+          image: "main3.png",
+          name: "Armine",
+          position: "Web Developer",
+        },
+        { id: 4, image: "main2.png", name: "Stefan", position: "CEO" },
+      ],
+      settings: {
+        itemsToShow: 3,
+        wrapAround: true,
+        breakpoints: {
+          10: {
+            itemsToShow: 1,
+            snapAlign: "center",
+          },
+          850: {
+            itemsToShow: 3,
+            snapAlign: "center",
+          },
+        },
+      },
+    };
+  },
+});
 </script>
 
-<style scoped lang="scss">
-
+<style scoped>
+.images {
+  padding-bottom: 50px;
+}
+.team-member {
+  text-align: center;
+}
+.member-image {
+  width: 250px;
+  height: 200px; 
+  object-fit: cover; 
+}
+.member-info {
+  padding: 10px;
+}
 </style>
--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -2,14 +2,16 @@
   <div id="about">
     <intro-sec />
     <team-members-sec />
+    <team-images />
   </div>
 </template>
 
 <script>
 import IntroSec from "@/components/sections/about/IntroSec.vue";
 import TeamMembersSec from "@/components/sections/about/TeamMembers.vue";
+import TeamImages from "@/components/sections/about/TeamImages.vue";
 export default {
-  components: { IntroSec, TeamMembersSec },
+  components: { IntroSec, TeamMembersSec, TeamImages },
   name: "AboutPage",
 };
 </script>
